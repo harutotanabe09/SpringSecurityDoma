f4fce37f91a5b0ac6ff2889654140e5c4385133a
Haruto Tanabe <harutotanabe@HarutonoMacBook-Air.local>
2019-07-24 22:18:50 +0900

    custom css add

--- src/main/java/app/sample/controller/html/NewsController.java
+++ src/main/java/app/sample/controller/html/NewsController.java
@@ -1,11 +1,12 @@
 package app.sample.controller.html;
 
-import app.sample.entity.LoginUserDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import app.sample.entity.LoginUserDetails;
 
 /** お知らせ画面のコントローラ. */
 @Controller
@@ -16,6 +17,6 @@ public class NewsController {
   @RequestMapping(method = RequestMethod.GET)
   public String init(Model model, @AuthenticationPrincipal LoginUserDetails userDetails) {
     model.addAttribute("loginInfo", userDetails.getUserInfo());
-    return "news/news";
+    return "news/tables";
   }
 }
